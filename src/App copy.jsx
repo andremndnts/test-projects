dc867bb203ed71de79a0154ada5e63b5900c1032
unknown <erandmndnts@gmail.com>
2026-03-26 19:15:37 +0800

    continuation of tutorial

---
+++ src/App copy.jsx
@@ -0,0 +1,281 @@
+import './App.css'
+import { useContext, useState } from 'react';
+import SignUpForm from './components/SignUpForm';
+import { Routes, Route, Link } from 'react-router-dom';
+import AuthContext from "./AuthContext";
+
+
+// CONTINUE TUTORIAL - 1:08:07 - https://www.youtube.com/watch?v=Wt3isV2irrA
+
+export default function App() {
+
+  const [user, setUser] = useState({name: "", isAuth: false});
+
+  function login(name){
+    // setUser=({name: name, isAuth: true})
+    setUser({ name: name, isAuth: true });
+  }
+
+  function logout(){
+
+    setUser({name: "", isAuth: false});
+  }
+
+
+  return ( 
+    <div> 
+
+
+    <AuthContext.Provider value={{ user, login, logout }}> 
+        <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/profile' element={<ProfilePage />}></Route>
+        <Route path='/login' element={<LoginPage />}></Route>
+        <Route path="*" element={<h1 style={{padding: "0 .15rem"}}>Not Found</h1>} />
+      </Routes>
+    </AuthContext.Provider>
+  
+    </div>
+  );
+}
+
+function Navbar(){
+
+  const {user, logout} = useContext(AuthContext);
+  
+  return (
+    <div>
+      <header style={{
+        padding: "1rem 1.5rem",
+        marginBottom: "1rem",
+        borderBottom: "1px solid #e5e7eb",
+        display: "flex",
+        justifyContent: "space-between"
+      }}>
+
+        <nav style={{display: "flex", gap: "1rem"}}>
+          <Link to="/">Home</Link>
+          <Link to="/profile">Profile</Link>
+        </nav>
+        
+        <div>
+          {!user.isAuth ? <Link to="/login">Login</Link> : <button onClick={logout}>Logout</button>}
+        </div>
+
+      </header>
+    </div>
+  )
+}
+
+
+
+function HomePage(){ 
+
+  const { user } = useContext(AuthContext);
+  
+  return (
+    <div style={{padding: "0 1.5rem"}}>
+      <h1>HOME</h1>
+
+      {user.isAuth ? (
+        <p>Welcome back, {user.name}</p>
+        ) : (
+        <p>You are not logged in. Go to the login page to sign in.</p>
+        )}
+
+    </div>
+  )
+}
+
+function ProfilePage(){
+
+  const {user} = useContext(AuthContext);
+
+  return (
+    <div style={{padding: "0 1.5rem"}}>
+
+      <h1>PROFILE PAGE</h1>
+      <p>Name: {user.name} </p> 
+      <p>Here you could show user info from the context</p>
+    </div>
+  )
+
+}
+
+function LoginPage(){
+
+  const [name, setName] = useState("");
+  // const [user, setUser] = useState({name: "", isAuth: false});
+  const { user, login} = useContext(AuthContext); 
+   
+
+  function handleSubmit(e){
+
+    e.preventDefault();
+    if(!name.trim()) return;
+      login(name);
+      // alert("login");
+  }
+
+  return (
+    <div style={{padding: "0 1.5rem"}}>
+      <h1>Login</h1>
+
+    <form onSubmit={handleSubmit} style={{marginTop: "1rem"}}>
+      <label>
+        Name
+
+        <input 
+          type="text"
+          placeholder="Type any name..."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          style={{marginLeft: "0.5rem"}}
+        />
+
+      </label>
+
+      <button type="submit" style={{marginLeft: "0.5rem"}}>
+        Log in
+      </button>
+
+    </form>
+
+      {user.isAuth && <p>User Logged In</p>}
+    </div>
+  )
+
+
+}
+
+
+
+
+// BASIC ROUTING
+// function App() {
+
+//   return (
+
+//     <div> 
+
+//       <nav style={{display: 'flex', gap: "1rem", marginBottom: "1rem"}}>
+//         <Link to="/">Home</Link>
+//         <Link to="/about">About</Link>
+//       </nav>
+
+//       <Routes>
+//         <Route path="/" element={<HomePage />}/>
+//         <Route path="/about" element={<AboutPage />}/>
+//         <Route path="*" element={<h1>404 Not Found</h1>} />
+//       </Routes>
+
+//       <div>Footer</div>
+
+//     </div>
+//   );
+// }
+
+
+
+function AboutPage(){
+  return <h1>ABOUT PAGE</h1>
+}
+
+
+
+// =========================== PREVIOUS TESTS HERE =============
+// function App() {
+
+//   // const workout = Muscle();
+//   // const name = "Erand";
+//   const age = 24;
+//   const greeting = "Good morning babyy! how's your sleep?";
+
+//   const [showGreeting, setShowGreeting] = useState("");
+
+//   const [name, setName] = useState(""); 
+
+//     // function toggleButtonGreeting(){
+
+//     //   if (showGreeting) {
+//     //     setShowGreeting(false);
+//     //   }else{
+//     //     setShowGreeting(true);
+//     //   }
+//     // }
+
+//     // function handleChange(event){
+//     //   const value = event.target.value;
+//     //   // console.log(value);
+//     //   setName(value);
+
+//     // } 
+
+
+//   return (
+
+//     <div> 
+     
+//       < SignUpForm />
+
+//       {/* < ToDoList/> */}
+      
+//       {/* <input type="text" placeholder="Enter password without typing" onChange={handleChange}/> */}
+
+//       {/* useState sample usage */}
+
+//         {/* <button onClick={toggleButtonGreeting}>Click Me</button> */}
+//         {/* {showGreeting &&
+//           <Greeting message={"Hi baby"} sender={"Erand"}/> 
+//         } */}
+
+//         {/* < Greeting message={"Good morning"} sender={"Anonymouse"}/>
+//         < Greeting message={"How's your sleep?"} sender={"KUYA"}/>
+//         < Greeting message={"Nice"} sender={"BOI"}/> */}
+
+//     </div>
+//   )
+// }
+
+// function ToDoList(){
+
+//   const todos = [
+//     {id: 1, text: "Eat Eggs"},
+//     {id: 2, text: "Go to the gym"},
+//     {id: 3, text: "Chat to baby, say i love you"}
+//   ]
+
+//   return (
+//     <div>
+//       <h2>TO DO LIST</h2>
+//       <ul>
+//         {todos.map((todo) => (
+
+//           <li key={todo.id}>{todo.text}</li>
+
+//         ))}
+//       </ul>
+
+//     </div>
+//   ) 
+
+// }
+
+
+// function Muscle(){
+//   return "Triceps";
+
+// }
+
+// function Greeting({message, sender}){
+
+//   return (
+//     <div>
+//       <h2>Hi! {message} from {sender}</h2>
+//     </div>        
+//   )
+// }
+
+
+// export default App
--- src/App.jsx
+++ src/App.jsx
@@ -1,262 +1,34 @@
-import './App.css'
-import { useContext, useState } from 'react';
-import SignUpForm from './components/SignUpForm';
-import { Routes, Route, Link } from 'react-router-dom';
-import AuthContext from "./AuthContext";
+import React, { useState, useEffect } from "react";
 
 
-// CONTINUE TUTORIAL - 1:08:07 - https://www.youtube.com/watch?v=Wt3isV2irrA
-
-export default function App() {
-
-  const [user, setUser] = useState({name: "", isAuth: false});
-
-
-  function login(name){
-    setUser=({name: name, isAuth: true})
-  }
-
-  return ( 
-    <div> 
-      <Navbar />
-
-    <AuthContext.Provider value={{ user, login }}> 
-      <Routes>
-        <Route path='/' element={<HomePage />}></Route>
-        <Route path='/profile' element={<ProfilePage />}></Route>
-        <Route path='/login' element={<LoginPage />}></Route>
-        <Route path="*" element={<h1 style={{padding: "0 .15rem"}}>Not Found</h1>} />
-      </Routes>
-    </AuthContext.Provider>
+export default function App(){
   
-    </div>
-  );
-}
+  const [count, setCount] = useState(0);
+  const [showCounter, setShowCounter] = useState(false);
+  // Mounting -> Updating -> Unmounting
 
-function Navbar(){
-  
-  return (
+  return(
     <div>
-      <header style={{
-        padding: "1rem 1.5rem",
-        marginBottom: "1rem",
-        borderBottom: "1px solid #e5e7eb",
-        display: "flex",
-        justifyContent: "space-between"
-      }}>
+    
+    <button onClick={() => setShowCounter(!showCounter)}>
+      {" "} 
+      Show counter
+    </button>
+    
+    {showCounter && <Counter />}
 
-        <nav style={{display: "flex", gap: "1rem"}}>
-          <Link to="/">Home</Link>
-          <Link to="/profile">Profile</Link>
-        </nav>
-        
-        <div>
-          <Link to="/login">Login</Link>
-        </div>
-
-      </header>
-    </div>
-  )
-}
-
-
-
-function HomePage(){
-  return (
-    <div style={{padding: "0 1.5rem"}}>
-      <h1>HOME</h1>
-
-      <p>You are not logged in. Go to the login page to sign in.</p>
-    </div>
-  )
-}
-
-function ProfilePage(){
-
-  return (
-    <div style={{padding: "0 1.5rem"}}>
-
-      <h1>PROFILE PAGE</h1>
-      <p>Name: [name here]</p> 
-      <p>Here you could show user info from the context</p>
     </div>
   )
 
 }
 
-function LoginPage(){
+function Counter(){
 
-  const [name, setName] = useState("");
-  // const [user, setUser] = useState({name: "", isAuth: false});
-  const { user, login} = useContext(AuthContext); 
-   
+  const [count, setCount] = useState(0); 
 
-  function handleSubmit(e){
+  useEffect(() => {
+    
+  })
 
-    e.preventDefault();
-    if(!name.trim()) return;
-      login(name);
-      // alert("login");
-  }
-
-  return (
-    <div style={{padding: "0 1.5rem"}}>
-      <h1>Login</h1>
-
-    <form onSubmit={handleSubmit} style={{marginTop: "1rem"}}>
-      <label>
-        Name
-
-        <input 
-          type="text"
-          placeholder="Type any name..."
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          style={{marginLeft: "0.5rem"}}
-        />
-
-      </label>
-
-      <button type="submit" style={{marginLeft: "0.5rem"}}>
-        Log in
-      </button>
-
-    </form>
-
-      {user.isAuth && <p>User Logged In</p>}
-    </div>
-  )
-
-
+  return <button onClick={() => setCount(count + 1)}>{count}</button>
 }
-
-
-
-
-// BASIC ROUTING
-// function App() {
-
-//   return (
-
-//     <div> 
-
-//       <nav style={{display: 'flex', gap: "1rem", marginBottom: "1rem"}}>
-//         <Link to="/">Home</Link>
-//         <Link to="/about">About</Link>
-//       </nav>
-
-//       <Routes>
-//         <Route path="/" element={<HomePage />}/>
-//         <Route path="/about" element={<AboutPage />}/>
-//         <Route path="*" element={<h1>404 Not Found</h1>} />
-//       </Routes>
-
-//       <div>Footer</div>
-
-//     </div>
-//   );
-// }
-
-
-
-function AboutPage(){
-  return <h1>ABOUT PAGE</h1>
-}
-
-
-
-// =========================== PREVIOUS TESTS HERE =============
-// function App() {
-
-//   // const workout = Muscle();
-//   // const name = "Erand";
-//   const age = 24;
-//   const greeting = "Good morning babyy! how's your sleep?";
-
-//   const [showGreeting, setShowGreeting] = useState("");
-
-//   const [name, setName] = useState(""); 
-
-//     // function toggleButtonGreeting(){
-
-//     //   if (showGreeting) {
-//     //     setShowGreeting(false);
-//     //   }else{
-//     //     setShowGreeting(true);
-//     //   }
-//     // }
-
-//     // function handleChange(event){
-//     //   const value = event.target.value;
-//     //   // console.log(value);
-//     //   setName(value);
-
-//     // } 
-
-
-//   return (
-
-//     <div> 
-     
-//       < SignUpForm />
-
-//       {/* < ToDoList/> */}
-      
-//       {/* <input type="text" placeholder="Enter password without typing" onChange={handleChange}/> */}
-
-//       {/* useState sample usage */}
-
-//         {/* <button onClick={toggleButtonGreeting}>Click Me</button> */}
-//         {/* {showGreeting &&
-//           <Greeting message={"Hi baby"} sender={"Erand"}/> 
-//         } */}
-
-//         {/* < Greeting message={"Good morning"} sender={"Anonymouse"}/>
-//         < Greeting message={"How's your sleep?"} sender={"KUYA"}/>
-//         < Greeting message={"Nice"} sender={"BOI"}/> */}
-
-//     </div>
-//   )
-// }
-
-// function ToDoList(){
-
-//   const todos = [
-//     {id: 1, text: "Eat Eggs"},
-//     {id: 2, text: "Go to the gym"},
-//     {id: 3, text: "Chat to baby, say i love you"}
-//   ]
-
-//   return (
-//     <div>
-//       <h2>TO DO LIST</h2>
-//       <ul>
-//         {todos.map((todo) => (
-
-//           <li key={todo.id}>{todo.text}</li>
-
-//         ))}
-//       </ul>
-
-//     </div>
-//   ) 
-
-// }
-
-
-// function Muscle(){
-//   return "Triceps";
-
-// }
-
-// function Greeting({message, sender}){
-
-//   return (
-//     <div>
-//       <h2>Hi! {message} from {sender}</h2>
-//     </div>        
-//   )
-// }
-
-
-// export default App
